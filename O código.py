ecd319dcf3899471793c77866416d5b26d2c4dc9
caiop3 <caio.ribeiro.p3@gmail.com>
2021-05-23 18:50:44 -0300

    Tentaiva de colar imagem

--- O código.py
+++ O código.py
@@ -1,2 +1,26 @@
 import pygame
 
+pygame.init()
+
+WIDTH = 500
+HEIGHT = 400
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Paraquedista')
+
+game = True
+
+image = pygame.image.load('montain.jpg').convert()
+image = pygame.transform.scale(image, (125, 166))
+
+while game:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game = False
+
+    window.fill((0, 0, 0))  
+    window.blit(image, (10, 10))    
+    
+    pygame.display.update()  
+
+pygame.quit()
