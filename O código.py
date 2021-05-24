33c342274eb55c266efc8ad8c11fa100960b8163
caiop3 <caio.ribeiro.p3@gmail.com>
2021-05-24 17:32:48 -0300

    Eagle locona

--- O código.py
+++ O código.py
@@ -7,14 +7,19 @@ HEIGHT = 400
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Paraquedista')
 
+
+image = pygame.image.load('C:/Users/caior/OneDrive/Documentos/Vs Code/PyGame/Referencia/montain.jpg').convert()
+image = pygame.transform.scale(image, (500, 400))
+parachute_img = pygame.image.load('C:/Users/caior/OneDrive/Documentos/Vs Code/PyGame/Referencia/parachute.png').convert_alpha()
+parachute_img = pygame.transform.scale(parachute_img, (120, 120))
+eagle_img = pygame.image.load('C:/Users/caior/OneDrive/Documentos/Vs Code/PyGame/Referencia/eagle.png').convert_alpha()
+eagle_img = pygame.transform.scale(eagle_img, (50, 50))
+
 game = True
 
-image = pygame.image.load('Referencia/montain.jpg').convert()
-image = pygame.transform.scale(image, (500, 400))
-parachute_img = pygame.image.load('Referencia/parachute.png').convert_alpha()
-parachute_img = pygame.transform.scale(parachute_img, (120, 120))
-eagle_img = pygame.image.load('Referencia/eagle.png').convert_alpha()
-eagle_img = pygame.transform.scale(eagle_img, (50, 50))
+eagle_x = -50
+eagle_y = 200
+eagle_speed = 3
 
 while game:
 
@@ -22,10 +27,17 @@ while game:
         if event.type == pygame.QUIT:
             game = False
 
+    eagle_x += eagle_speed
+    eagle_y += eagle_speed
+
+    if eagle_y > HEIGHT:
+        eagle_x = -50
+        eagle_y = 200
+
     window.fill((0, 0, 0))  
     window.blit(image, (0, 0))    
-    window.blit(parachute_img, (210, 30))
-    window.blit(eagle_img, (100, 30))
+    #window.blit(parachute_img, (210, 30))
+    window.blit(eagle_img, (eagle_x, eagle_y))
 
     pygame.display.update()  
 
