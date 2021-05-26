3c171ee60cfe39ecebbe676d2f7294003ebe3ff1
ArthurPansini <arthurpansini35@gmail.com>
2021-05-26 15:29:04 -0300

    Adicionando todas as classes

--- O código.py
+++ O código.py
@@ -1,3 +1,4 @@
+
 import pygame
 import random
 
@@ -8,32 +9,69 @@ HEIGHT = 400
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Paraquedista')
 
-image = pygame.image.load('C:/Users/caior/OneDrive/Documentos/Vs Code/PyGame/Referencia/montain.jpg').convert()
+image = pygame.image.load('Referencia/montain.jpg').convert()
 image = pygame.transform.scale(image, (500, 400))
 
-parachute_img = pygame.image.load('C:/Users/caior/OneDrive/Documentos/Vs Code/PyGame/Referencia/parachute.png').convert_alpha()
+parachute_img = pygame.image.load('Referencia/parachute.png').convert_alpha()
 parachute_img = pygame.transform.scale(parachute_img, (120, 120))
 
-eagle_img = pygame.image.load('C:/Users/caior/OneDrive/Documentos/Vs Code/PyGame/Referencia/eagle.png').convert_alpha()
-eagle_img = pygame.transform.scale(eagle_img, (50, 50))
+eagle1_img = pygame.image.load('Referencia/eagle2.png').convert_alpha()
+eagle1_img = pygame.transform.scale(eagle1_img, (50, 50))
 
-eagle2_img = pygame.image.load('C:/Users/caior/OneDrive/Documentos/Vs Code/PyGame/Referencia/eagle2.png').convert_alpha()
+eagle2_img = pygame.image.load('Referencia/eagle.png').convert_alpha()
 eagle2_img = pygame.transform.scale(eagle2_img, (50, 50))
+
+class Eagle1(pygame.sprite.Sprite):
+    def __init__(self, img):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.x = -50
+        self.rect.y = random.randint(200, 300)
+        self.speed_x = random.randint(2, 6)
+        self.speed_y = random.randint(-7, -1)
+    
+    def update(self):
+
+        self.rect.x += self.speed_x
+        self.rect.y += self.speed_y
+
+        if self.rect.top > 400 or self.rect.left > 500:
+            self.rect.x = -50
+            self.rect.y = random.randint(200, 300)
+            self.speed_x = random.randint(2, 6)
+            self.speed_y = random.randint(-7, -1)
+
+class Eagle2(pygame.sprite.Sprite):
+    def __init__(self, img):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.x = 550
+        self.rect.y = random.randint(200, 300)
+        self.speed_x = random.randint(2, 6)
+        self.speed_y = random.randint(-7, -1)
+    
+    def update(self):
+
+        self.rect.x -= self.speed_x
+        self.rect.y += self.speed_y
+
+        if self.rect.top > 400 or self.rect.right < -50:
+            self.rect.x = 550
+            self.rect.y = random.randint(200, 300)
+            self.speed_x = random.randint(2, 6)
+            self.speed_y = random.randint(-7, -1)
 
 game = True
 
-eagle_x = 550
-eagle_y = random.randint(50, 300)
-eagle_speed_x = random.randint(3, 6)
-eagle_speed_y = random.randint(-2, 2)
-
-eagle2_x = -50
-eagle2_y = random.randint(50, 300)
-eagle2_speed_x = random.randint(3, 7)
-eagle2_speed_y = random.randint(-2, 2)
-
 clock = pygame.time.Clock()
 FPS = 30
+
+eagle1 = Eagle1(eagle1_img)
+eagle2 = Eagle2(eagle2_img)
 
 while game:
     clock.tick(FPS)
@@ -42,30 +80,19 @@ while game:
         if event.type == pygame.QUIT:
             game = False
 
-    eagle_x -= eagle_speed_x
-    eagle_y += eagle_speed_y 
-    eagle2_x += eagle2_speed_x
-    eagle2_y += eagle2_speed_y 
-
-    if eagle_x < -50 or eagle_y > 450 or eagle_y < -50: 
-        eagle_x = 550
-        eagle_y = random.randint(50, 300)
-        eagle_speed_x = random.randint(3, 6)
-        eagle_speed_y = random.randint(-2, 2)
-
-    if eagle2_x > 500 or eagle2_y > 450 or eagle2_y < -50:
-        eagle2_x = -50
-        eagle2_y = random.randint(50, 300)
-        eagle2_speed_x = random.randint(3, 6)
-        eagle2_speed_y = random.randint(-2, 2)
+    eagle1.update()
+    eagle2.update()
 
     window.fill((0, 0, 0))  
     window.blit(image, (0, 0))    
     #window.blit(parachute_img, (210, 30))
-    window.blit(eagle_img, (eagle_x, eagle_y))
-    window.blit(eagle2_img, (eagle2_x, eagle2_y))
+    window.blit(eagle1.image, eagle1.rect)
+    window.blit(eagle2.image, eagle2.rect)
 
     pygame.display.update()  
 
 pygame.quit()
+
+
+
 
