a71e10c16f7241c931f15e131face5d0b8a71816
caiop3 <caio.ribeiro.p3@gmail.com>
2021-06-05 10:08:39 -0300

    Finalmente uma animação

--- O código.py
+++ O código.py
@@ -3,6 +3,9 @@
 import pygame
 import random
 import time
+from os import path
+
+img_dir = path.join(path.dirname(__file__), 'Efeitos')
 
 pygame.init()
 
@@ -33,7 +36,7 @@ assets['covid_img'] = pygame.image.load('Efeitos/covid.png').convert_alpha()
 assets['covid_img'] = pygame.transform.scale(assets['covid_img'], (20, 20))
 
 assets['gel_img'] = pygame.image.load('Efeitos/gel.png').convert_alpha()
-assets['gel_img'] = pygame.transform.scale(assets['gel_img'], (35, 35))
+assets['gel_img'] = pygame.transform.scale(assets['gel_img'], (20, 20))
 
 # --- Importa o som de fundo
 pygame.mixer.music.load('Efeitos/musica.mp3')
@@ -163,11 +166,100 @@ class Gel(pygame.sprite.Sprite):
     def update(self):
         self.rect.y += self.speedy
 
+def load_spritesheet(spritesheet, rows, columns):
+    # Calcula a largura e altura de cada sprite.
+    sprite_width = spritesheet.get_width() // columns
+    sprite_height = spritesheet.get_height() // rows
+    
+    # Percorre todos os sprites adicionando em uma lista.
+    sprites = []
+    for row in range(rows):
+        for column in range(columns):
+            # Calcula posição do sprite atual
+            x = column * sprite_width
+            y = row * sprite_height
+            # Define o retângulo que contém o sprite atual
+            dest_rect = pygame.Rect(x, y, sprite_width, sprite_height)
+
+            # Cria uma imagem vazia do tamanho do sprite
+            image = pygame.Surface((sprite_width, sprite_height), pygame.SRCALPHA)
+            # Copia o sprite atual (do spritesheet) na imagem
+            image.blit(spritesheet, (0, 0), dest_rect)
+            sprites.append(image)
+    return sprites
+STILL = 0
+# Classe Jogador que representa o herói
+class Player(pygame.sprite.Sprite):
+    
+    # Construtor da classe. O argumento player_sheet é uma imagem contendo um spritesheet.
+    def __init__(self, player_sheet):
+        
+        # Construtor da classe pai (Sprite).
+        pygame.sprite.Sprite.__init__(self)
+        
+        # Aumenta o tamanho do spritesheet para ficar mais fácil de ver
+        player_sheet = pygame.transform.scale(player_sheet, (640, 640))
+
+        # Define sequências de sprites de cada animação
+        spritesheet = load_spritesheet(player_sheet, 8, 5)
+        self.animations = {STILL: spritesheet[0:39]}
+        # Define estado atual (que define qual animação deve ser mostrada)
+        self.state = STILL
+        # Define animação atual
+        self.animation = self.animations[self.state]
+        # Inicializa o primeiro quadro da animação
+        self.frame = 0
+        self.image = self.animation[self.frame]
+        # Detalhes sobre o posicionamento.
+        self.rect = self.image.get_rect()
+        
+        # Centraliza na tela.
+        self.rect.centerx = WIDTH / 2
+        self.rect.centery = HEIGHT / 2
+
+        # Guarda o tick da primeira imagem
+        self.last_update = pygame.time.get_ticks()
+
+        # Controle de ticks de animação: troca de imagem a cada self.frame_ticks milissegundos.
+        self.frame_ticks = 30
+        
+    # Metodo que atualiza a posição do personagem
+    def update(self):
+        # Verifica o tick atual.
+        now = pygame.time.get_ticks()
+
+        # Verifica quantos ticks se passaram desde a ultima mudança de frame.
+        elapsed_ticks = now - self.last_update
+
+        # Se já está na hora de mudar de imagem...
+        if elapsed_ticks > self.frame_ticks:
+
+            # Marca o tick da nova imagem.
+            self.last_update = now
+
+            # Avança um quadro.
+            self.frame += 1
+
+            # Atualiza animação atual
+            self.animation = self.animations[self.state]
+            # Reinicia a animação caso o índice da imagem atual seja inválido
+            # if self.frame >= len(self.animation):
+            #     self.frame = 0
+            
+            # Armazena a posição do centro da imagem
+            center = self.rect.center
+            # Atualiza imagem atual
+            self.image = self.animation[self.frame]
+            # Atualiza os detalhes de posicionamento
+            self.rect = self.image.get_rect()
+            self.rect.center = center
+
 # --- Cria um grupo de sprites geral e para cada obstáculo
 all_sprites = pygame.sprite.Group()
 aguias = pygame.sprite.Group()
 covides = pygame.sprite.Group()
 gels = pygame.sprite.Group()
+
 groups = {}
 groups['all_sprites'] = all_sprites
 groups['aguias'] = aguias
@@ -197,6 +289,8 @@ game = True
 # --- Variável para o ajuste da velocidade
 clock = pygame.time.Clock()
 FPS = 25
+
+player_sheet = pygame.image.load(path.join(img_dir, 'ex.png')).convert_alpha()
 
 # ===== LOOP PRRINCIPAL =====    
 while game:
@@ -238,12 +332,14 @@ while game:
         m = Eagle1(assets)
         all_sprites.add(m)
         aguias.add(m)  
-
+    
         balao.kill()
+        player = Player(player_sheet)
+        all_sprites.add(player)
         boom_sound.play()
         pygame.mixer.music.stop()
-        game = False
-        time.sleep(2)
+        # game = False
+        # time.sleep(2)
     
     colisao2 = pygame.sprite.spritecollide(balao, aguias, True)
     for aguia in colisao2:
@@ -252,10 +348,12 @@ while game:
         aguias.add(m)
 
         balao.kill()
+        player = Player(player_sheet)
+        all_sprites.add(player)
         boom_sound.play()
         pygame.mixer.music.stop()
-        game = False
-        time.sleep(2)
+        # game = False
+        # time.sleep(2)
     
     colisao3 = pygame.sprite.spritecollide(balao, covides, True)
     for covid in colisao3:
@@ -298,5 +396,6 @@ while game:
 
 # ===== FINALIZAÇÃO =====
 pygame.quit()  # Finaliza os recursos utilizados
+
 
 
